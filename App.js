b3b6df8864726ebaab2b14a4d8f2467562475612
Matsumoto Tsubasa <>
2020-12-21 22:08:32 +0900

    feat: add GetNews Api

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, View, FlatList, SafeAreaView, Alert } from "react-native";
 import ListItem from "./components/ListItem";
 import dummyArticles from "./dummies/article";
+import Constants from "expo-constants";
+import axios from "axios";
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
 
 const styles = StyleSheet.create({
   container: {
@@ -33,14 +37,21 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function App() {
+export default App = () => {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setArticles(dummyArticles);
-    }, 3000);
-    return () => clearTimeout(timer);
+    fetchArticles();
   }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      console.log(response);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
@@ -56,4 +67,4 @@ export default function App() {
       />
     </SafeAreaView>
   );
-}
+};
