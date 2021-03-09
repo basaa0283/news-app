58fc241a3264050f49343ed947690e6ad3b1f6ca
Matsumoto Tsubasa <>
2021-03-09 11:54:36 +0900

    feat: add redux code

--- App.js
+++ App.js
@@ -1,39 +1,12 @@
 import React from "react";
-import { StyleSheet, View, FlatList, SafeAreaView, Alert } from "react-native";
-import Constants from "expo-constants";
 import AppNavigator from "./navigation/AppNavigator";
-
-const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    borderColor: "gray",
-    borderWidth: 1,
-    flexDirection: "row",
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    padding: 10,
-    justifyContent: "space-between",
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray",
-  },
-});
+import { Provider } from "react-redux";
+import store from "./store";
 
 export default App = () => {
-  return <AppNavigator />;
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 };
